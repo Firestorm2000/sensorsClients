e732491eebff99aa435d6f1f30f89495fb0f32d4
Firestorm2000 <teenwolf2000@abv.bg>
2019-11-24 15:13:08 +0200

    only left working app

--- radina/fuel/app/views/reports/delete.php
+++
@@ -1,8 +0,0 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "create" ); ?>'><?php echo Html::anchor('reports/create','Create');?></li>
-	<li class='<?php echo Arr::get($subnav, "update" ); ?>'><?php echo Html::anchor('reports/update','Update');?></li>
-	<li class='<?php echo Arr::get($subnav, "read" ); ?>'><?php echo Html::anchor('reports/read','Read');?></li>
-	<li class='<?php echo Arr::get($subnav, "delete" ); ?>'><?php echo Html::anchor('reports/delete','Delete');?></li>
-
-</ul>
-<p>Delete</p>
--- radina/fuel/app/views/sensor/delete.php
+++
@@ -1,8 +0,0 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "create" ); ?>'><?php echo Html::anchor('sensor/create','Create');?></li>
-	<li class='<?php echo Arr::get($subnav, "delete" ); ?>'><?php echo Html::anchor('sensor/delete','Delete');?></li>
-	<li class='<?php echo Arr::get($subnav, "update" ); ?>'><?php echo Html::anchor('sensor/update','Update');?></li>
-	<li class='<?php echo Arr::get($subnav, "read" ); ?>'><?php echo Html::anchor('sensor/read','Read');?></li>
-
-</ul>
-<p>Delete</p>
